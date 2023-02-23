3e659a53c99fdf7b32336b786026a0cd619468ca
Alexy Martinet <amlds.dev@gmail.com>
2023-02-23 10:13:23 +0100

    validation inscription form

--- src/components/FormLogin.tsx
+++ src/components/FormLogin.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 const FormLogin: React.FC = () => {
   const [isLogin, setIsLogin] = React.useState(true);
+  const messagePassword = React.useRef<HTMLParagraphElement>(null);
+  const messagePasswordValidate = React.useRef<HTMLParagraphElement>(null);
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -9,48 +11,26 @@ const FormLogin: React.FC = () => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
     console.log(data);
+    messagePassword.current!.innerHTML = '';
+    messagePasswordValidate.current!.innerHTML = '';
     if (!isLogin) {
       if(!passwordValidation(data.password)) {
-        alert('Email invalide');
+        messagePassword.current!.innerHTML = '(Il faut 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial)';
         return;
       }
-      console.log('login');
-    } else {
-      console.log('inscription');
+      if(!passwordConfirmValidation(data.password, data.passwordConfirm)) {
+        messagePasswordValidate.current!.innerHTML = '(Mot de passe pas indentique)';
+        return;
+      }
     }
-
-    //fait la validation des champs du formulaire
-/*     if (isLogin) {
-      if (!emailValidation(data.email)) {
-        alert('Email invalide');
-        return;
-      }
-      if (!passwordValidation(data.password)) {
-        alert('Mot de passe invalide');
-        return;
-      }
-    } else {
-      if (!usernameValidation(data.username)) {
-        alert('Username invalide');
-        return;
-      }
-      if (!emailValidation(data.email)) {
-        alert('Email invalide');
-        return;
-      }
-      if (!passwordValidation(data.password)) {
-        alert('Mot de passe invalide');
-        return;
-      }
-      if (!passwordConfirmValidation(data.password, data.passwordConfirm)) {
-        alert('Les mots de passe ne correspondent pas');
-        return;
-      }
-    } */
-
+    window.location.href = '/dashboard';
   };
 
   React.useEffect(() => {
+    const form = document.querySelector('form');
+    if (form) {
+      form.reset();
+    }
     const button = document.querySelector('.button--link');
     if (button) {
       button.addEventListener('click', () => {
@@ -59,23 +39,11 @@ const FormLogin: React.FC = () => {
     }
   }, [isLogin]);
 
-  //validatation mot de passe inscription, il faut 8 caractères, 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial
   const passwordValidation = (password: any) => {
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return regex.test(password);
   };
-
-  const emailValidation = (email: any) => {
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(email);
-  };
-
-  const usernameValidation = (username: string) => {
-    const regex = /^[a-zA-Z0-9]+$/;
-    return regex.test(username);
-  };
-
-  const passwordConfirmValidation = (password: string, passwordConfirm: string) => {
+  const passwordConfirmValidation = (password: any, passwordConfirm: any) => {
     return password === passwordConfirm;
   };
 
@@ -87,11 +55,12 @@ const FormLogin: React.FC = () => {
       <label htmlFor="email">Email
         <input className='input--txt' type="email" name="email" id="email-input" placeholder="LeWagonLaitier@lewagon.com" />
       </label>
-      <label htmlFor="password">Password
-        <input className='input--txt' type="password" name="password" id="password-input" placeholder="·········" />
+      <label htmlFor="password">Password <span ref={messagePassword} className='messageError'></span>
+        <input className='input--txt' type="password" name="new-password" id="password-input" placeholder="·········" />
+
       </label>
-      <label htmlFor="password">Confirm password
-        <input className='input--txt' type="password" name="password" id="password-input" placeholder="·········" />
+      <label htmlFor="password">Confirm password <span ref={messagePasswordValidate} className='messageError'></span>
+        <input className='input--txt' type="password" name="new-passwordValidate" id="password-validate-input" placeholder="·········" />
       </label>
       <button className='button--primary' type="submit">Sign up</button>
       <p className='alignText'>Already register ? <button className='button--link md-text-1'>Log in here</button></p>
