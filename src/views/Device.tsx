a4046258d22a23a6df467886790c404dbf0e8717
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 14:09:11 +0100

    add course batch page Device

--- src/views/Device.tsx
+++ src/views/Device.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import EventService from '../services/EventService';
 import Event from '../types/Event';
+import BatchService from '../services/BatchService';
+import Batch from '../types/Batch';
 
 import ListCard from '../components/ListCard';
+import ChallengeView from '../components/ChallengesView';
 
 import '../assets/views/device.scss'
 
@@ -12,9 +15,15 @@ const getEvents = async () => {
   return events;
 }
 
+const getBatchs = async (batchId: number) => {
+  const batch = await BatchService.getBatchs(batchId);
+  return batch;
+}
+
 const Devices: React.FC = () => {
   const messageRef = React.useRef<HTMLParagraphElement>(null);
   const [events, setEvents] = React.useState<Event[]>([]);
+  const [batch, setBatch] = React.useState<Batch[]>([]);
   const [date, setDate] = React.useState<Date>(new Date());
 
   React.useEffect(() => {
@@ -28,6 +37,20 @@ const Devices: React.FC = () => {
       setEvents(filteredEvents);
     });
   }, []);
+
+  React.useEffect(() => {
+    getBatchs(1).then((batch) => {
+      console.log(batch);
+      const sortedBatch = batch.sort((a, b) => {
+        return new Date(a.start_at).getTime() - new Date(b.start_at).getTime();
+      });
+      const filteredBatch = sortedBatch.filter((batch) => {
+        return new Date(batch.start_at).getTime() > new Date().getTime();
+      });
+      setBatch(filteredBatch);
+    });
+  }, []);
+
 
   React.useEffect(() => {
     if(messageRef.current)
@@ -49,6 +72,7 @@ const Devices: React.FC = () => {
           <p>Ici une phase que Marina pourra changer à sa guise</p>
           <div className="align-row">
             <p>Aujourd’hui au programme :</p>
+            {batch.map((batch) => {return (<ChallengeView key={batch.number}CourseId={batch.course_id} />)})}
           </div>
         </div>
       </section>
