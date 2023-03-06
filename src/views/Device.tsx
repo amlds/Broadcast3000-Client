d4c8f826d7247911c42d3de375f58b45bc1757e0
Alexy Martinet <amlds.dev@gmail.com>
2023-03-06 10:53:41 +0100

    clean old code

--- src/views/Device.tsx
+++ src/views/Device.tsx
@@ -15,53 +15,18 @@ const getEvents = async () => {
 }
 
 const Devices: React.FC = () => {
-  /* const [date, setDate] = React.useState(new Date());
-  const [dayMonth, setDayMonth] = React.useState(date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'numeric', day: 'numeric'})); */
   const messageRef = React.useRef<HTMLParagraphElement>(null);
-
-  React.useEffect(() => {
-   /*  getEvents().then((events) => {
-      const sortedEvents = events.sort((a, b) => {
-        return new Date(a.endEvent).getTime() - new Date(b.endEvent).getTime();
-      });
-      const filteredEvents = sortedEvents.filter((event) => {
-        return new Date(event.endEvent).getTime() > new Date().getTime();
-      });
-      setEvents(filteredEvents);
-    }); */
-    getEvents().then((events) => {
-      setEvents(events);
-    });
-  }, []);
-
-  /* React.useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date());
-      setDayMonth(date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'numeric', day: 'numeric'}));
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [date]);
-
-  React.useEffect(() => {
-    if (messageRef.current) {
-      messageRef.current.innerHTML = date.toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', weekday: 'long',});
-    }
-  }, [date]);
- */
   const [events, setEvents] = React.useState<Event[]>([]);
 
   React.useEffect(() => {
     getEvents().then((events) => {
       const sortedEvents = events.sort((a, b) => {
-        return new Date(a.end_time).getTime() - new Date(b.end_time).getTime();
+        return new Date(a.start_time).getTime() - new Date(b.start_time).getTime();
       });
       const filteredEvents = sortedEvents.filter((event) => {
-        return new Date(event.end_time).getTime() > new Date().getTime();
+        return new Date(event.start_time).getTime() > new Date().getTime();
       });
       setEvents(filteredEvents);
-    });
-    getEvents().then((events) => {
-      setEvents(events);
     });
   }, []);
 
@@ -72,8 +37,6 @@ const Devices: React.FC = () => {
         <div className='device__content--text'>
           <h1>Hello wagoners !</h1>
           <p>Nous somme le <span className='text-medium' ref={messageRef}></span></p>
-          {/* <LectureDay date={dayMonth}/> */}
-          <FocusImage />
         </div>
       </section>
       <ListCard events={events} />
