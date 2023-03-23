2ae1593dcceb1491fbced3826ecfee1df949db37
Alexy Martinet <amlds.dev@gmail.com>
2023-03-23 21:11:38 +0100

    modify interface device

--- src/types/device.ts
+++ src/types/device.ts
@@ -1,17 +1,17 @@
 export default interface Device {
-  "id"?: number,
-  "school": number,
-  "events": {
-    "id": number,
-    "name": string,
-    "description": string,
-    "start_date": string,
-    "end_date": string,
-    "location": string,
-    "event_type": {
-      "id": number,
-      "name": string,
-      "description": string
+  id?: number,
+  school: number,
+  events: {
+    id: number,
+    name: string,
+    description: string,
+    start_date: string,
+    end_date: string,
+    image: string,
+    event_type: {
+      id: number,
+      name: string,
+      description: string
     }
   }
 }
