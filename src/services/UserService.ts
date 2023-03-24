fcac706e4a254c08ee6967249865d14055d5c0ae
Alexy Martinet <amlds.dev@gmail.com>
2023-03-24 09:22:39 +0100

    create service user

---
+++ src/services/UserService.ts
@@ -0,0 +1,31 @@
+import User from '../types/User';
+
+const url = 'http://localhost:3000/api/v1';
+
+const UserService = {
+  async connectUser(user: User): Promise<User> {
+    const response = await fetch(`${url}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    const newUser = await response.json();
+    return newUser;
+  },
+
+  async createUser(user: User): Promise<User> {
+    const response = await fetch(`${url}/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    const newUser = await response.json();
+    return newUser;
+  }
+};
+
+export default UserService;
