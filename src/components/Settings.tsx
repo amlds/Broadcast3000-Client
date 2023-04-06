6d22c15375052a058162effadadec13ed2fc4b2c
Alexy Martinet <amlds.dev@gmail.com>
2023-04-06 18:07:52 +0200

    add btn disconect

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { TokenContext } from '../context/TokenContext';
 
 import BatchConfig from './BatchConfig';
 
@@ -13,6 +15,14 @@ import BatchConfig from './BatchConfig';
   }
 }*/
 const Settings: React.FC = () => {
+  const { token , setToken } = React.useContext(TokenContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    setToken('');
+    navigate('/login');
+  }
+
   return (
     <section className='settings'>
       {/*<BatchConfig />*/}
@@ -37,12 +47,12 @@ const Settings: React.FC = () => {
       </div>
       <div className="settings__sadBad">
         <button className="button--secondary">You’re sad ? Click here !</button>
-        <Link to="/" className='buttonLink button--secondary button--secondary--red'>
+        <button className='button--secondary button--secondary--red' onClick={handleClick}>
           Log out !
           <svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11.75 7V3.25C11.75 2.65326 11.5129 2.08097 11.091 1.65901C10.669 1.23705 10.0967 1 9.5 1H3.5C2.90326 1 2.33097 1.23705 1.90901 1.65901C1.48705 2.08097 1.25 2.65326 1.25 3.25V16.75C1.25 17.3467 1.48705 17.919 1.90901 18.341C2.33097 18.7629 2.90326 19 3.5 19H9.5C10.0967 19 10.669 18.7629 11.091 18.341C11.5129 17.919 11.75 17.3467 11.75 16.75V13M8 7L5 10M5 10L8 13M5 10H17.75" stroke="#E60F05" />
           </svg>
-        </Link>
+        </button>
       </div>
     </section>
   );
